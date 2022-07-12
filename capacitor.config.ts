8b5df938ed9842fed9967800a1dcd02ebd3dd4b9
rtran3 <78782055+rtran3@users.noreply.github.com>
2022-07-12 14:24:34 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'a4.app',
+  appName: 'a4',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
